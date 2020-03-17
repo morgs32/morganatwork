2ad9c174f066247caa409cf840ff4ce6478b6105
Morgan Intrator <morgan.intrator@gmail.com>
2020-03-16 18:44:27 -0600

    fix: spelling error

--- app/cms/products/stackshirts/meta.js
+++ app/cms/products/stackshirts/meta.js
@@ -5,7 +5,7 @@ module.exports = {
   role: 'maker',
   productLabels: ['hobbyProject'],
   website: 'http://www.stackshirts.com',
-  shortDescription: `Love your tech stach? Design the layout of
+  shortDescription: `Love your tech stack? Design the layout of
    your favorite stack and have it printed on a shirt.`,
   logo: require('./v2LikeIcon.svg'),
   heroImage: require('./Stackshirts.png')
