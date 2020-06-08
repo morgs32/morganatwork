1ea15dd2e320274cbec09eb01384dc92f4ce77f6
Morgan Intrator <morgan.intrator@gmail.com>
2020-06-08 19:36:39 -0400

    fix: comments out ThemeToggle.tsx

--- src/components/ThemeToggle/ThemeToggle.tsx
+++ src/components/ThemeToggle/ThemeToggle.tsx
@@ -1,40 +1,39 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
 ThemeToggle.propTypes = {};
 ThemeToggle.defaultProps = {};
 
-export default function ThemeToggle(props) {
+export default function ThemeToggle() {
   return (
     <div>
-      <Toggle
-        icons={{
-          checked: (
-            <img
-              src={moon}
-              width="16"
-              height="16"
-              role="presentation"
-              style={{ pointerEvents: 'none' }}
-            />
-          ),
-          unchecked: (
-            <img
-              src={sun}
-              width="16"
-              height="16"
-              role="presentation"
-              style={{ pointerEvents: 'none' }}
-            />
-          ),
-        }}
-        checked={this.state.theme === 'dark'}
-        onChange={e =>
-          window.__setPreferredTheme(
-            e.target.checked ? 'dark' : 'light'
-          )
-        }
-      />
+      {/*<Toggle*/}
+      {/*  icons={{*/}
+      {/*    checked: (*/}
+      {/*      <img*/}
+      {/*        src={moon}*/}
+      {/*        width="16"*/}
+      {/*        height="16"*/}
+      {/*        role="presentation"*/}
+      {/*        style={{ pointerEvents: 'none' }}*/}
+      {/*      />*/}
+      {/*    ),*/}
+      {/*    unchecked: (*/}
+      {/*      <img*/}
+      {/*        src={sun}*/}
+      {/*        width="16"*/}
+      {/*        height="16"*/}
+      {/*        role="presentation"*/}
+      {/*        style={{ pointerEvents: 'none' }}*/}
+      {/*      />*/}
+      {/*    ),*/}
+      {/*  }}*/}
+      {/*  checked={this.state.theme === 'dark'}*/}
+      {/*  onChange={e =>*/}
+      {/*    window.__setPreferredTheme(*/}
+      {/*      e.target.checked ? 'dark' : 'light'*/}
+      {/*    )*/}
+      {/*  }*/}
+      {/*/>*/}
     </div>
   );
 }
