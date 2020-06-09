6f37dd01c5f2d5efa4d8b4f6aad0760681a838b2
Morgan Intrator <morgan.intrator@gmail.com>
2020-06-09 13:37:32 -0400

    feat: adds post on sourcemaps and preval macros

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,9 @@
 module.exports = function (api) {
   api.cache(process.env.NODE_ENV === 'production');
   return {
-
     presets: ['next/babel'],
     plugins: [
+      'macros',
       '@babel/plugin-syntax-top-level-await',
       'babel-plugin-styled-components',
       'import-glob-array',
---
+++ src/utils/allDigests.ts
@@ -0,0 +1,29 @@
+import preval from 'babel-plugin-preval/macro';
+
+const files: Array<string> = preval`
+  const path = require('path')
+  const fs = require('fs')
+  const DIR = path.join(process.cwd(), "./pages/digests/");
+  const files = fs
+    .readdirSync(DIR)
+    .filter(file => file.endsWith(".md") || file.endsWith(".mdx"))
+    
+  module.exports = files
+`;
+
+module.exports = files.map((file) => {
+  const {meta = {}, default: page} = require(`../../pages/digests/${file}`);
+
+  const date = file.split('.')[0]
+
+  return {
+    pathname: `digests/${date}`,
+    type: 'digest',
+    date,
+    title: meta.title || 'Newsworthy',
+    headings: meta.headings || page({}).props.children.filter(child => child.props.originalType === 'h2').map((h2) => {
+      return h2.props.children;
+    }),
+    ...meta,
+  };
+})
